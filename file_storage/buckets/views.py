363944288a40a25fa35bba7756097cdc96d8de47
kingsley okpara <kingsconsult001@gmail.com>
2020-02-05 14:20:09 +0100

    created the view all api endpoints, the test and views.

--- file_storage/buckets/views.py
+++ file_storage/buckets/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+from .models import Bucket
+from . import models
+from .serializers import BucketSerializer
+from . import serializers
+
+class BucketList(APIView):
+    def get(self, request):
+        buckets = Bucket.objects.all()
+        return Response({"buckets": buckets})
