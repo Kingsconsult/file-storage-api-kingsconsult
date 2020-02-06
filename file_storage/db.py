d926b22423a52240fb35357cf900ff69d8a14875
kingsley okpara <kingsconsult001@gmail.com>
2020-02-06 20:27:52 +0100

    update settings

---
+++ file_storage/db.py
@@ -0,0 +1,30 @@
+import dj_database_url
+from decouple import config
+
+
+class DB:
+    @classmethod
+    def config(cls, debug):
+        return cls.development() if debug else cls.production()
+
+    @classmethod
+    def production(cls):
+        return {
+            'default': {
+                'ENGINE': 'djongo',
+                'NAME': 'file-storage',
+                'HOST': 'db:27017',
+            }
+        }
+
+    @classmethod
+    def development(cls):
+        return {
+            'default': {
+                'ENGINE': 'mongo',
+                'NAME': 'file-storage',
+                'HOST': 'db',
+                'USER': config('DBUSER'),
+                'PASSWORD': config('DBPASSWD'),
+            }
+        }
